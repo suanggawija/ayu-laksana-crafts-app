5d1308fb3aa1939b3d0ab25083a25701203d8d98
suanggawija <suanggawija@gmail.com>
2025-05-08 19:06:19 +0800

    feat: generate user dashboard page

---
+++ src/app/(dashboard)/dashboard/product/page.tsx
@@ -0,0 +1,15 @@
+import { Table } from "@/components/layouts/dashboard/product";
+import React from "react";
+
+const Product = () => {
+  return (
+    <main>
+      <section>
+        <div className="p-3 bg-white shadow-sm rounded-lg">p</div>
+      </section>
+      <Table />
+    </main>
+  );
+};
+
+export default Product;
